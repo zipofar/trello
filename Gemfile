d35742738f1013223ea21964b45d0b23a0f53ba1
Zipofar <zipofar@gmail.com>
2020-03-29 19:31:03 +0300

    Add state machine to task

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'bcrypt', '~> 3.1.7'
 gem 'simple_form'
 gem 'state_machines'
 gem 'state_machines-activerecord'
+gem 'aasm'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,38 @@
 class Task < ApplicationRecord
+  include AASM
+
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
+
+  validates name:, presence: true
+  validates description:, presence: true
+  validates description:, length: { maximum: 500 }
+  validates author_id:, presence: true
+
+  aasm do
+    state :new_task, initial: true
+
+    event :to_development do
+      transitions from: [:new_task, :in_code_review, :in_qa], to: :in_development
+    end
+
+    event :to_qa do
+      transitions from: :in_development, to: :in_qa
+    end
+
+    event :to_code_review do
+      transitions from: :in_qa, to: :in_code_review
+    end
+
+    event :to_ready_for_release do
+      transitions from: :in_code_review, to: :ready_for_release
+    end
+
+    event :release do
+      transitions from: :ready_for_release, to: :released
+    end
+
+    event :archive do
+      transitions from: [:new_task, :released], to: :archived
+    end
 end
